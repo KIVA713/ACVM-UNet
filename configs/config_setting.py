1654c99ad64793f980e85f52f0d968b1ee27aad8
KIVA713 <18656745958@163.com>
2026-04-21 21:41:02 +0800

    Add files via upload

---
+++ configs/config_setting.py
@@ -0,0 +1,137 @@
+from torchvision import transforms
+from utils import *
+
+from datetime import datetime
+
+class setting_config:
+    """
+    the config of training setting.
+    """
+
+    network = 'vmunet'
+    model_config = {
+        'num_classes': 1, 
+        'input_channels': 3, 
+        # ----- VM-UNet ----- #
+        'depths': [2,2,2,2],
+        'depths_decoder': [2,2,2,1],
+        'drop_path_rate': 0.1,
+        'load_ckpt_path': './pre_trained_weights/vmamba_small_e238_ema.pth',
+    }
+
+    datasets = 'isic18' 
+    if datasets == 'isic18':
+        data_path = ''
+    elif datasets == 'isic17':
+        data_path = ''
+
+    else:
+        raise Exception('datasets in not right!')
+
+    criterion = BceDiceLoss(wb=1, wd=1)
+
+    pretrained_path = './pre_trained/'
+    num_classes = 1
+    input_size_h = 256
+    input_size_w = 256
+    input_channels = 3
+    distributed = False
+    local_rank = -1
+    num_workers = 0
+    seed = 42
+    world_size = None
+    rank = None
+    amp = False
+    gpu_id = '0'
+    batch_size = 16
+    epochs = 150
+
+    work_dir = 'results/' + network + '_' + datasets + '_' + datetime.now().strftime('%A_%d_%B_%Y_%Hh_%Mm_%Ss') + '/'
+
+    print_interval = 20
+    val_interval = 1
+    save_interval = 100
+    threshold = 0.5
+    only_test_and_save_figs = False
+    best_ckpt_path = 'PATH_TO_YOUR_BEST_CKPT'
+    img_save_path = 'PATH_TO_SAVE_IMAGES'
+
+    train_transformer = transforms.Compose([
+        myNormalize(datasets, train=True),
+        myToTensor(),
+        myRandomHorizontalFlip(p=0.5),
+        myRandomVerticalFlip(p=0.5),
+        myRandomRotation(p=0.5, degree=[0, 360]),
+        myResize(input_size_h, input_size_w)
+    ])
+    test_transformer = transforms.Compose([
+        myNormalize(datasets, train=False),
+        myToTensor(),
+        myResize(input_size_h, input_size_w)
+    ])
+
+    opt = 'AdamW'
+    assert opt in ['Adadelta', 'Adagrad', 'Adam', 'AdamW', 'Adamax', 'ASGD', 'RMSprop', 'Rprop', 'SGD'], 'Unsupported optimizer!'
+    if opt == 'Adadelta':
+        lr = 0.01
+        rho = 0.9
+        eps = 1e-6
+        weight_decay = 0.05
+    elif opt == 'Adagrad':
+        lr = 0.01
+        lr_decay = 0
+        eps = 1e-10
+        weight_decay = 0.05
+    elif opt == 'Adam':
+        lr = 0.001
+        betas = (0.9, 0.999)
+        eps = 1e-8
+        weight_decay = 0.0001
+        amsgrad = False
+    elif opt == 'AdamW':
+        lr = 1e-4 # default: 1e-3 – learning rate
+        betas = (0.9, 0.999) # default: (0.9, 0.999) – coefficients used for computing running averages of gradient and its square
+        eps = 1e-8 # default: 1e-8 – term added to the denominator to improve numerical stability
+        weight_decay = 1e-4 # default: 1e-2 – weight decay coefficient
+        amsgrad = False
+
+    
+    sch = 'WP_CosineLR'
+    if sch == 'StepLR':
+        step_size = epochs // 5 # – Period of learning rate decay.
+        gamma = 0.5 # – Multiplicative factor of learning rate decay. Default: 0.1
+        last_epoch = -1 # – The index of last epoch. Default: -1.
+    elif sch == 'MultiStepLR':
+        milestones = [60, 120, 150] # – List of epoch indices. Must be increasing.
+        gamma = 0.1 # – Multiplicative factor of learning rate decay. Default: 0.1.
+        last_epoch = -1 # – The index of last epoch. Default: -1.
+    elif sch == 'ExponentialLR':
+        gamma = 0.99 #  – Multiplicative factor of learning rate decay.
+        last_epoch = -1 # – The index of last epoch. Default: -1.
+    elif sch == 'CosineAnnealingLR':
+        
+        T_max = 150 # – Maximum number of iterations. Cosine function period.
+        eta_min = 0.00001 # – Minimum learning rate. Default: 0.
+        last_epoch = -1 # – The index of last epoch. Default: -1.  
+    elif sch == 'ReduceLROnPlateau':
+        mode = 'min' # – One of min, max. In min mode, lr will be reduced when the quantity monitored has stopped decreasing; in max mode it will be reduced when the quantity monitored has stopped increasing. Default: ‘min’.
+        factor = 0.1 # – Factor by which the learning rate will be reduced. new_lr = lr * factor. Default: 0.1.
+        patience = 10 # – Number of epochs with no improvement after which learning rate will be reduced. For example, if patience = 2, then we will ignore the first 2 epochs with no improvement, and will only decrease the LR after the 3rd epoch if the loss still hasn’t improved then. Default: 10.
+        threshold = 0.0001 # – Threshold for measuring the new optimum, to only focus on significant changes. Default: 1e-4.
+        threshold_mode = 'rel' # – One of rel, abs. In rel mode, dynamic_threshold = best * ( 1 + threshold ) in ‘max’ mode or best * ( 1 - threshold ) in min mode. In abs mode, dynamic_threshold = best + threshold in max mode or best - threshold in min mode. Default: ‘rel’.
+        cooldown = 0 # – Number of epochs to wait before resuming normal operation after lr has been reduced. Default: 0.
+        min_lr = 0 # – A scalar or a list of scalars. A lower bound on the learning rate of all param groups or each group respectively. Default: 0.
+        eps = 1e-08 # – Minimal decay applied to lr. If the difference between new and old lr is smaller than eps, the update is ignored. Default: 1e-8.
+    elif sch == 'CosineAnnealingWarmRestarts':
+        T_0 = 50 # – Number of iterations for the first restart.
+        T_mult = 2 # – A factor increases T_{i} after a restart. Default: 1.
+        eta_min = 1e-6 # – Minimum learning rate. Default: 0.
+        last_epoch = -1 # – The index of last epoch. Default: -1. 
+    elif sch == 'WP_MultiStepLR':
+        warm_up_epochs = 10
+        gamma = 0.1
+        milestones = [125, 225]
+    elif sch == 'WP_CosineLR':
+        warm_up_epochs = 10
+        T_max = 300
+        eta_min = 1e-6
